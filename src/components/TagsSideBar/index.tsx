d84c1dcd0edccbf15a5ac9df35c1b5a5180a2887
Anastasiia <nastyadedela@gmail.com>
2024-05-08 20:19:27 +0200

    feat: added content sections and styles for them

---
+++ src/components/TagsSideBar/index.tsx
@@ -0,0 +1,3 @@
+export default function TagsSideBar() {
+  return <div>SideBar</div>;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
+import TagsSideBar from '@/components/TagsSideBar';
 import styles from './Home.module.scss';
+import BlogList from '@/components/BlogList';
 
 export default function Home() {
   return (
@@ -8,7 +10,10 @@ export default function Home() {
           <h1>glossa</h1>
           <p>A place to share your knowledge.</p>
         </div>
-        <div className="content"></div>
+        <div className={styles.content}>
+          <BlogList />
+          <TagsSideBar />
+        </div>
       </div>
     </main>
   );
