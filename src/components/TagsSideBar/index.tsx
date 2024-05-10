7efe125573d7f2338789b32366997fe75d2f57ce
Anastasiia <nastyadedela@gmail.com>
2024-05-10 19:30:11 +0200

    feat: completed sidebar markup and styles

--- src/components/TagsSideBar/index.tsx
+++ src/components/TagsSideBar/index.tsx
@@ -1,3 +1,23 @@
+import styles from './TagsSideBar.module.scss';
+
 export default function TagsSideBar() {
-  return <div>SideBar</div>;
+  return (
+    <div className={styles.tagsContainer}>
+      <div>
+        <p className={styles.tagsTitle}>Popular Tags</p>
+        <div className={styles.tagsList}>
+          <button>eos</button>
+          <button>est</button>
+          <button>ipsum</button>
+          <button>enim</button>
+          <button>repellat</button>
+          <button>quia</button>
+          <button>consequatur</button>
+          <button>facilis</button>
+          <button>exercitationem</button>
+          <button>tenetur</button>
+        </div>
+      </div>
+    </div>
+  );
 }
