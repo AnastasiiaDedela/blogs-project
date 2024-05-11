954cf37d71478a8d124aac8af48efbdee95ece22
Anastasiia <nastyadedela@gmail.com>
2024-05-11 16:52:00 +0200

    feat: completed pagination markup and styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
 import TagsSideBar from '@/components/TagsSideBar';
 import styles from './Home.module.scss';
 import BlogList from '@/components/BlogList';
+import { useState } from 'react';
+import Pagination from '@/components/Pagination';
 
 export default function Home() {
+  const [page, setPage] = useState<number>(1);
   return (
     <main>
       <div className={styles.container}>
@@ -14,6 +17,7 @@ export default function Home() {
           <BlogList />
           <TagsSideBar />
         </div>
+        <Pagination page={page} setPage={(number) => setPage(number)} />
       </div>
     </main>
   );
