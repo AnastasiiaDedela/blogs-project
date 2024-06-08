c174f2ba3de63463db79b414ba69a565488180b6
Anastasiia <nastyadedela@gmail.com>
2024-06-08 18:44:19 +0200

    feat: post request for adding articles

--- src/pages/AddArticle/index.tsx
+++ src/pages/AddArticle/index.tsx
@@ -1,27 +1,56 @@
 import { useState } from 'react';
 import styles from './AddArticle.module.scss';
+import axios from 'axios';
 
 export default function AddArticle() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [tags, setTags] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Article Submitted:', { title, content });
-    setTitle('');
-    setContent('');
+  const handleTagsChange = (e) => {
+    setTags(e.target.value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const tagsArray = tags
+      .split(',')
+      .map((tag) => tag.trim())
+      .filter((tag) => tag);
+
+    const articleData = {
+      text: content,
+      title: title,
+      tags: tagsArray,
+    };
+
+    const token = localStorage.getItem('@token');
+
+    axios
+      .post('http://localhost:8001/api/posts', articleData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log('Article added:', res.data);
+        setTitle('');
+        setContent('');
+        setTags('');
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error('There was an error adding the article:', error);
+      });
+  };
   return (
-    <div>
-      <h2>Add New Article</h2>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.div}>
-          <label className={styles.label} htmlFor="title">
-            Title:
-          </label>
+    <div className={styles.addArticleContainer}>
+      <h2 className={styles.title}>Add New Article</h2>
+      <form onSubmit={handleSubmit} className={styles.addArticleForm}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="title">Title:</label>
           <input
-            className={styles.input}
             type="text"
             id="title"
             value={title}
@@ -29,20 +58,22 @@ export default function AddArticle() {
             required
           />
         </div>
-        <div className={styles.div}>
-          <label className={styles.label} htmlFor="content">
-            Content:
-          </label>
+        <div className={styles.inputContainer}>
+          <label htmlFor="content">Content:</label>
           <textarea
-            className={styles.textarea}
+            className={styles.contentInput}
             id="content"
             value={content}
-            onChange={(e) => setContent(e.target.value)}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
             required></textarea>
         </div>
-        <button className={styles.button} type="submit">
-          Add Article
-        </button>
+        <div className={styles.inputContainer}>
+          <label htmlFor="tags">Tags:</label>
+          <textarea id="tags" value={tags} onChange={handleTagsChange} required />
+        </div>
+        <button type="submit">Add Article +</button>
       </form>
     </div>
   );
