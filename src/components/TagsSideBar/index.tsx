bb49f3bff19c9b71d64ff1092744fea545925d63
Anastasiia <nastyadedela@gmail.com>
2024-08-11 17:49:02 +0200

    feat: add tags to sidebar

--- src/components/TagsSideBar/index.tsx
+++ src/components/TagsSideBar/index.tsx
@@ -2,25 +2,35 @@ import { Link } from 'react-router-dom';
 import styles from './TagsSideBar.module.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function TagsSideBar() {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8001/api/posts/tags`)
+      .then((res) => {
+        setTags(res.data.tags);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div className={styles.sideBar}>
       <div className={styles.tagsContainer}>
         <div>
           <p className={styles.tagsTitle}>Popular Tags</p>
           <div className={styles.tagsList}>
-            <button>eos</button>
-            <button>est</button>
-            <button>ipsum</button>
-            <button>enim</button>
-            <button>repellat</button>
-            <button>quia</button>
-            <button>consequatur</button>
-            <button>facilis</button>
-            <button>exercitationem</button>
-            <button>tenetur</button>
+            {tags.map((tag) => (
+              <Link to="/">
+                <p className={styles.tag}>{tag}</p>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
