c682e38725f8a74cac083354ab666e28a5f4a361
Anastasiia <nastyadedela@gmail.com>
2024-08-25 18:19:55 +0200

    feat: refactor requests for AuthorPage

---
+++ src/components/first.tsx
@@ -0,0 +1,22 @@
+// import { useFetch } from './useFetch';
+
+// type User = {
+//   id: number;
+//   name: string;
+// };
+
+// const first = () => {
+//   const { isLoading, data: users, error } = useFetch('url..');
+
+//   if (isLoading) {
+//     return <div>Loading..</div>;
+//   }
+
+//   if (error != null) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return <div>{users && users.map((user) => <div>{user.id}</div>)}</div>;
+// };
+
+// export default first;
