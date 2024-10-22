84f1266aee48b138537b3252a8f0172b03eb2340
Anastasiia <nastyadedela@gmail.com>
2024-10-22 20:46:39 +0200

    fix: fix refetching comments list after adding new comment

---
+++ src/components/AddCommentBlock/CommentsSection.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import styles from './CommentsSection.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { addComment, getComments } from '@/services/commentsServices';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { CommentData } from '@/types/comments';
+import CommentItem from '../CommentItem/CommentItem';
+interface CommentsProps {
+  postId: number;
+  limit: number;
+  offset: number;
+}
+
+const AddComment = ({ postId, limit, offset }: CommentsProps) => {
+  const [newComment, setNewComment] = useState('');
+  const [commentsList, setCommentsList] = useState<CommentData[]>([]);
+
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem('@token') || '';
+
+  const handleAddComment = async () => {
+    if (!isLoggedIn) {
+      console.error('User is not authenticated. Redirect to sign-in.');
+      navigate('/sign-in');
+      return;
+    }
+    const response = await addComment(postId, newComment, token);
+    setCommentsList((prev) => [response, ...prev]);
+    setNewComment('');
+  };
+
+  useEffect(() => {
+    getComments(postId, limit, offset)
+      .then((res) => {
+        setCommentsList(res.items);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className={styles.commentsWrapper}>
+      <div className={styles.commentForm}>
+        <form className={styles.editForm}>
+          <label>
+            <i>Leave your comment</i>
+            <textarea
+              onChange={(e) => setNewComment(e.target.value)}
+              className={styles.textarea}
+              value={newComment}
+            />
+          </label>
+          <div className={styles.submitBtn}>
+            {newComment.length > 0 && (
+              <button type="button" onClick={handleAddComment}>
+                Comment
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+      <div className={styles.commentsList}>
+        {commentsList &&
+          commentsList.map((comment) => <CommentItem comment={comment} key={comment.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default AddComment;
