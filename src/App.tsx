fdc723cb569ad468cbf1aee07d8a3a04a78887c4
Anastasiia <nastyadedela@gmail.com>
2024-11-09 15:43:15 +0100

    fix: fix autologin after page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { useDispatch } from 'react-redux';
+import { useQuery } from '@tanstack/react-query';
+import { getMe } from './services/usersServices';
+import { login } from './redux/slices/auth/slice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem('@token') || '';
+
+  const { data: user, isSuccess } = useQuery({
+    queryKey: ['user'],
+    queryFn: getMe,
+  });
+
+  useEffect(() => {
+    if (isSuccess && user) {
+      dispatch(login({ token, user }));
+    }
+  }, [isSuccess, user, dispatch]);
   return (
     <>
       <RouterProvider router={router} />
