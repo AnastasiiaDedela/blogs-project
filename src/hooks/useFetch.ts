dd008cd44c3bdbfdbecc61ff32a3f4d5584b10be
Anastasiia <nastyadedela@gmail.com>
2024-11-18 23:42:43 +0100

    delete: delete custom hook useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,30 +1,30 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 
-export const useFetch = <T>(url: string, dependencies: Array<number | string | string[]> = []) => {
-  const [data, setData] = useState<T | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const token = localStorage.getItem('@token');
+// export const useFetch = <T>(url: string, dependencies: Array<number | string | string[]> = []) => {
+//   const [data, setData] = useState<T | null>(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+//   const token = localStorage.getItem('@token');
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, dependencies);
+//   useEffect(() => {
+//     setIsLoading(true);
+//     axios
+//       .get(url, {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       })
+//       .then((res) => {
+//         setData(res.data);
+//       })
+//       .catch((error) => {
+//         setError(error);
+//       })
+//       .finally(() => {
+//         setIsLoading(false);
+//       });
+//   }, dependencies);
 
-  return { data: data, isLoading: isLoading, error: error };
-};
+//   return { data: data, isLoading: isLoading, error: error };
+// };
